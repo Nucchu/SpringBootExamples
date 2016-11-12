a05aee88defde7853fabc701d3c53900e6440367
nchendu@gmail.com <Lucky@2014>
2016-11-13 00:52:22 +0530

    Removed the commnetedCOde...test commit

--- SpringBootBasicDemo/src/main/java/hello/Application.java
+++ SpringBootBasicDemo/src/main/java/hello/Application.java
@@ -12,24 +12,7 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//        return args -> {
-//
-//            System.out.println("Let's inspect the beans provided by Spring Boot:");
-//
-//            String[] beanNames = ctx.getBeanDefinitionNames();
-//            Arrays.sort(beanNames);
-//            for (String beanName : beanNames) {
-//                System.out.println(beanName);
-//            }
-//
-//        };
-//    }
-//    @ExceptionHandler(Exception.class)
-//    void handleBadRequests(HttpServletResponse response) throws IOException {
-//        response.sendError(HttpStatus.BAD_REQUEST.value(), "Please try again and with a non empty string as 'name'");
-//    }
+ 
     @Bean  // Magic entry 
     public DispatcherServlet dispatcherServlet() {
         DispatcherServlet ds = new DispatcherServlet();
